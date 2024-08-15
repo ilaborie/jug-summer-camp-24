2fad679c665ca732720bdf619e2e8f28efb8c8f7
igor <ilaborie@gmail.com>
2024-08-15 07:34:46 +0200

    feat: 🧬 Dioxus App

--- dioxus-wc/src/lib.rs
+++ dioxus-wc/src/lib.rs
@@ -5,27 +5,57 @@ use wasm_bindgen::prelude::*;
 
 #[component]
 pub fn App() -> Element {
-    //     let mut current = use_signal(Option::<usize>::default);
-    //     let on_count = move |event: CountEvent| {
-    //         current.set(Some(event.count));
-    //     };
+    let mut current = use_signal(Option::<usize>::default);
+
+    let on_count = move |event: CountEvent| {
+        current.set(Some(event.count));
+    };
 
     rsx! {
-       h1  {
-            "🧬 Hello Dioxus"
+        div { class: "container",
+            h1 { "🧬 Hello Dioxus" }
+
+            Counter {
+                name: "Plop",
+                on_count,
+            }
+
+            if let Some(count) = current() {
+                code { "{count}" }
+            }
         }
     }
 }
 
-// pub struct CountEvent {
-//     pub count: usize,
-// }
+#[derive(Debug, Clone, Copy)]
+pub struct CountEvent {
+    pub count: usize,
+}
 
 // const STYLE: InjectedStyle = InjectedStyle::css(include_str!("counter.css"));
 
-// FIXME Counter component
+/// Counter component
+#[component]
+pub fn Counter(name: Option<String>, on_count: EventHandler<CountEvent>) -> Element {
+    let mut count = use_signal(usize::default);
+
+    let name = name.unwrap_or_default();
+
+    let onclick = move |_event| {
+        count += 1;
+        on_count.call(CountEvent { count: count() });
+    };
+
+    rsx! {
+        label { "{name}"}
+        button {
+            onclick,
+            "{count}"
+        }
+    }
+}
 
 // #[wasm_bindgen(start)]
-// fn start() {
+// pub fn start() {
 //     register_counter();
 // }
