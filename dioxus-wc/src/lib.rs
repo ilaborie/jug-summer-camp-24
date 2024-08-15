956b1372212aa80d47d6129989148994bd761a94
igor <ilaborie@gmail.com>
2024-08-15 07:34:46 +0200

    feat: 🧬 Dioxus App

--- dioxus-wc/src/lib.rs
+++ dioxus-wc/src/lib.rs
@@ -2,10 +2,54 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn App() -> Element {
+    let mut count_event = use_signal(Option::<usize>::default);
+
+    let on_count = move |event: CountEvent| {
+        count_event.set(Some(event.count));
+    };
 
     rsx! {
-       h1  {
-            "🧬 Hello Dioxus"
+        div { class: "container",
+            div {
+                display: "flex",
+                align_items: "center",
+                gap: "1rem",
+                Counter {
+                    name: "plop",
+                    on_count,
+                }
+                code {
+                    if let Some(count) = count_event() {
+                        "{count}"
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct CountEvent {
+    pub count: usize,
+}
+
+/// Counter component
+#[component]
+pub fn Counter(name: Option<String>, on_count: EventHandler<CountEvent>) -> Element {
+    let mut count = use_signal(usize::default);
+
+    let name = name.unwrap_or_default();
+
+    let onclick = move |_event| {
+        count += 1;
+        on_count.call(CountEvent { count: count() });
+    };
+
+    rsx! {
+        label { "{name}"}
+        button {
+            onclick,
+            "{count}"
         }
     }
 }
